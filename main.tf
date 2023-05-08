04c6c9ac55ee5b8e3e9abbad6046f02fca0cd5bf
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2023-05-08 10:49:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-provider "random" {
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
 }
 
+provider "null" {
+  # Configuration options
+}
 resource "random_string" "test" {
   length           = 16
   special          = true
